ab2916d05aa3d0fbfee33acf48c45d12302198ab
Syeda Ayesha <113896482+Syeda-Ayesha@users.noreply.github.com>
2023-01-18 11:04:34 +0530

    Update text_summarizer.py

--- text_summarizer.py
+++ text_summarizer.py
@@ -3,6 +3,8 @@ import streamlit as st
 import spacy
 nlp=spacy.load('en_core_web_sm')
 from spacy import displacy
+import nltk
+nltk.download('punkt')
 HTML_WRAPPER="""<div style="overflow-x:auto; border:1px solid #e6e9ef;border-radius:0.25rem; padding:1rem">{}</div>"""
 
 #[theme]
