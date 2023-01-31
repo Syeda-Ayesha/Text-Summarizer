c646376bc6bf288db5d8ba51b819d4dc80cf34c3
Syeda Ayesha <113896482+Syeda-Ayesha@users.noreply.github.com>
2023-01-31 10:25:07 +0530

    Update text_summarizer.py

--- text_summarizer.py
+++ text_summarizer.py
@@ -123,7 +123,8 @@ def main():
                 st.info("Length::Full Text:: {}".format(len_of_full_text))
                 st.info("length::Short Text:: {}".format(len_of_short_text))
                 output1 = result[:len_of_short_text] # final_result_from_processing_the_input
-                st.text_area(label="Preview:", value=output1, height=200)
+                st.write("### **Preview:**")
+                st.text_area(label="## blue[**Preview:**]", value=output1, height=200)
                 summary_docx=sumy_summarizer(result)
                 docx=analyze_text(summary_docx)
                 html=displacy.render(docx,style='ent')
